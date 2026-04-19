450ac375ab19d5a267a1e601d7ada4bad5296647
Bedarshi Baruah <baruahbedarshi949@gmail.com>
2026-04-19 12:19:06 +0530

    Update axios.js

--- frontend888/src/api/axios.js
+++ frontend888/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://infotact-project1-e9it.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 // 🔐 Attach token automatically
