b972915fa690a14b3d366ce94ec0ed66f1669999
Bedarshi Baruah <baruahbedarshi949@gmail.com>
2026-04-19 11:27:35 +0530

    Update axios.js

--- frontend888/src/api/axios.js
+++ frontend888/src/api/axios.js
@@ -17,21 +17,3 @@ API.interceptors.request.use((req) => {
 
 export default API;
 
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
-
-// // 🔐 ADD TOKEN AUTOMATICALLY
-// API.interceptors.request.use((req) => {
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return req;
-// });
-
-// export default API;
